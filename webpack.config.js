3cebf3acdde660800b71797d238ac62155c590c1
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2020-10-12 22:41:36 +0530

    feat(sw): service worker config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, 'src/index.js'),
@@ -15,6 +17,12 @@ module.exports = {
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'src/index.html'),
+        }),
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true,
         }),
     ],
     output: {
